232d9561ef5fa5b183805777bce5a6a43a4fac37
Ikhwanul Hakim <ikh.hakim@gmail.com>
2020-05-20 20:15:19 +0700

    penambahan fitur akar

--- hitung.js
+++ hitung.js
@@ -5,3 +5,14 @@ function kali(x,y) {
 function bagi(x,y){
     return x / y
 }
+
+function tambah(x,y){
+    return x + y
+}
+
+
+function akar(x, n = 2){
+    if (n == 2) return Math.sqrt(x)
+    else if (n == 3) return Math.cbrt(x)
+    else return x
+}
